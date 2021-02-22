3e8b51833942a7aab1f8632fcc7a9ec284a22609
unknown <dorabbb45@gmail.com>
2021-02-22 02:23:47 -0500

    tarea

---
+++ java.js
@@ -0,0 +1,13 @@
+function sucesion ( ){
+    v =  prompt ( "Ingrese un Numero" );
+      
+      for ( i=1; i<=v ; i++) {
+                 
+                 for( j=1;j<=i;j++) {
+                 num= document.write(i+ ",")
+                 }
+     }
+     alert(v);
+     
+}
+sucesion ( );
